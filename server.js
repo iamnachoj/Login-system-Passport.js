31ca9c2ee4e2690f97982002e840d29038c06015
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-07-04 10:34:30 +0200

    update app.listen in order to make it work in Heroku

--- server.js
+++ server.js
@@ -92,4 +92,7 @@ function checkNotAuthenticated(req, res, next) {
 }
 
 //server listen
-app.listen(3000, () => console.log("server running on port 3000"));
+const port = process.env.PORT || 8080;
+app.listen(port, "0.0.0.0", () => {
+  console.log("Listening on Port: " + port);
+});
